a2282c9af24e8dd8afc02f138c31738c4d223843
santiago0411 <santicarnuccio@gmail.com>
2021-08-19 17:01:22 -0300

    ReadFile bug fix

--- Hazel/src/Platform/OpenGL/OpenGLShader.cpp
+++ Hazel/src/Platform/OpenGL/OpenGLShader.cpp
@@ -51,13 +51,23 @@ namespace Hazel
 	{
 		std::string result;
 		std::ifstream in(filepath, std::ios::in | std::ios::binary);
-		size_t size = in.tellg();
-		if (size != -1)
+		
+		if (in)
 		{
-			result.resize(size);
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			in.seekg(0, std::ios::end);
+			size_t size = in.tellg();
+
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], result.size());
+				in.close();
+			}
+			else
+			{
+				HZ_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
