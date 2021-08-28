4911603f4e44064d72b6c124bfd9e226436c80c0
santiago0411 <santicarnuccio@gmail.com>
2021-08-28 17:37:52 -0300

    Parsed function name in Instrumentor to remove __cdecl from __FUNCSIG__

--- Hazel/src/Hazel/Debug/Instrumentor.h
+++ Hazel/src/Hazel/Debug/Instrumentor.h
@@ -66,19 +66,16 @@ namespace Hazel
 		{
 			std::stringstream json;
 
-			std::string name = result.Name;
-			std::ranges::replace(name, '"', '\'');
-
 			json << std::setprecision(3) << std::fixed;
-			json << ",{";
-			json << "\"cat\":\"function\",";
-			json << "\"dur\":" << (result.ElapsedTime.count()) << ',';
-			json << "\"name\":\"" << name << "\",";
-			json << "\"ph\":\"X\",";
-			json << "\"pid\":0,";
-			json << "\"tid\":" << result.ThreadId << ",";
-			json << "\"ts\":" << result.Start.count();
-			json << "}";
+			json << R"(,{)";
+			json << R"("cat":"function",)";
+			json << R"("dur":)" << result.ElapsedTime.count() << ",";
+			json << R"("name":")" << result.Name << R"(",)";
+			json << R"("ph":"X",)";
+			json << R"("pid":0,)";
+			json << R"("tid":)" << result.ThreadId << ",";
+			json << R"("ts":)" << result.Start.count();
+			json << R"(})";
 
 			std::lock_guard lock(m_Mutex);
 			if (m_CurrentSession)
@@ -97,13 +94,13 @@ namespace Hazel
 	private:
 		void WriteHeader()
 		{
-			m_OutputStream << "{\"otherData\": {},\"traceEvents\":[{}";
+			m_OutputStream << R"({"otherData": {},"traceEvents":[{})";
 			m_OutputStream.flush();
 		}
 
 		void WriteFooter()
 		{
-			m_OutputStream << "]}";
+			m_OutputStream << R"(]})";
 			m_OutputStream.flush();
 		}
 
@@ -155,6 +152,37 @@ namespace Hazel
 		bool m_Stopped;
 		std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
 	};
+
+	namespace InstrumentorUtils
+	{
+		template<size_t N>
+		struct ChangeResult
+		{
+			char Data[N];
+		};
+
+		template<size_t N, size_t K>
+		constexpr auto CleanupOutputString(const char(&expr)[N], const char(&remove)[K])
+		{
+			ChangeResult<N> result{};
+
+			size_t srcIndex = 0;
+			size_t dstIndex = 0;
+			while (srcIndex < N)
+			{
+				size_t matchIndex = 0;
+				while (matchIndex < K - 1 && srcIndex + matchIndex < N - 1 && expr[srcIndex + matchIndex] == remove[matchIndex])
+					matchIndex++;
+				if (matchIndex == K - 1)
+					srcIndex += matchIndex;
+				result.Data[dstIndex++] = expr[srcIndex] == '"' ? '\'' : expr[srcIndex];
+				srcIndex++;
+			}
+
+			return result;
+		}
+		
+	}
 }
 
 #define HZ_PROFILE 1
@@ -166,7 +194,7 @@ namespace Hazel
 		#define HZ_FUNC_SIG __PRETTY_FUNCTION__
 	#elif defined(__DMC__) && (__DMC__ >= 0x810)
 		#define HZ_FUNC_SIG __PRETTY_FUNCTION__
-	#elif defined(__FUNCSIG__)
+	#elif (defined(__FUNCSIG__) || (_MSC_VER))
 		#define HZ_FUNC_SIG __FUNCSIG__
 	#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 		#define HZ_FUNC_SIG __FUNCTION__
@@ -182,7 +210,8 @@ namespace Hazel
 
 	#define HZ_PROFILE_BEGIN_SESSION(name, filepath)	::Hazel::Instrumentor::Get().BeginSession(name, filepath)
 	#define HZ_PROFILE_END_SESSION()					::Hazel::Instrumentor::Get().EndSession()
-	#define HZ_PROFILE_SCOPE(name)						::Hazel::InstrumentationTimer time##__LINE__(name);
+	#define HZ_PROFILE_SCOPE(name) constexpr auto fixedName = ::Hazel::InstrumentorUtils::CleanupOutputString(name, "__cdecl ");\
+														::Hazel::InstrumentationTimer time##__LINE__(fixedName.Data)
 	#define HZ_PROFILE_FUNCTION()						HZ_PROFILE_SCOPE(HZ_FUNC_SIG)
 #else
 	#define HZ_PROFILE_BEGIN_SESSION(name, filepath)
