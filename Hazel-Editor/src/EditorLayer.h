91e992cedc6b4b1815fba6836eb12628d75560bc
santiago0411 <santicarnuccio@gmail.com>
2021-09-07 22:27:36 -0300

    Open/Save file dialogs

--- Hazel-Editor/src/EditorLayer.h
+++ Hazel-Editor/src/EditorLayer.h
@@ -18,7 +18,12 @@ namespace Hazel
 		void OnUpdate(Timestep ts) override;
 		void OnImGuiRender() override;
 		void OnEvent(Event& e) override;
+	private:
+		bool OnKeyPressed(KeyPressedEvent& e);
 
+		void NewScene();
+		void OpenScene();
+		void SaveSceneAs();
 	private:
 		OrthographicCameraController m_CameraController;
 
