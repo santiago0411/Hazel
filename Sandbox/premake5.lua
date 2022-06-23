bad3a2eae03fd399c0eaa7390f1ccc5e3bdf62d7
santiago0411 <santicarnuccio@gmail.com>
2022-06-23 22:38:57 +0900

    Changed Vulkan version to 1.3.216 and removed project local debug libraries

--- Sandbox/premake5.lua
+++ Sandbox/premake5.lua
@@ -35,11 +35,6 @@ project "Sandbox"
 		runtime "Debug"
 		symbols "on"
 
-		postbuildcommands
-		{
-			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
-		}
-
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
 		runtime "Release"
